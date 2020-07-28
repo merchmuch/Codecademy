8826dcd9a72fde7206a80d0968ac8fdc39c2059d
merchmuch <merchmuch@gmail.com>
2020-07-28 16:12:51 -0400

    Initial commit

---
+++ data_structure.py
@@ -0,0 +1,58 @@
+class Node:
+    def __init__(self, value, next_node=None):
+        self.value = value
+        self.next_node = next_node
+
+    def __repr__(self):
+        return "A node with value {}. ".format(self.value)
+
+    def set_next_node(self, next_node):
+        self.next_node = next_node
+
+    def get_next_node(self):
+        return self.next_node
+
+    def get_value(self):
+        return self.value
+
+class LinkedList:
+    def __init__(self, value=None):
+        self.head_node = Node(value)
+
+    def get_head_node(self):
+        return self.head_node
+
+    def insert_beginning(self, new_value):
+        new_node = Node(new_value)
+        new_node.set_next_node(self.head_node)
+        self.head_node = new_node
+
+    def stringify_list(self):
+        string_list = ""
+        current_node = self.get_head_node()
+        while current_node:
+            if current_node.get_value() != None:
+                string_list += str(current_node.get_value()) + "\n"
+            current_node = current_node.get_next_node()
+        return string_list
+
+    def remove_node(self, value_to_remove):
+        current_node = self.get_head_node()
+        if current_node.get_value() == value_to_remove:
+            self.head_node = current_node.get_next_node()
+        else:
+            while current_node:
+                next_node = current_node.get_next_node()
+                if next_node.get_value() == value_to_remove:
+                    current_node.set_next_node(next_node.get_next_node())
+                    current_node = None
+                else:
+                    current_node = next_node
+
+node_a = Node(1)
+node_b = Node(2)
+node_c = Node(3)
+print(node_c.get_value())
+print(node_b.get_next_node())
+ll = LinkedList(0)
+print(ll.get_head_node())
